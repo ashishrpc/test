1cb1f80b071b1ab668794f232e8d357f9630aee2
Ashish ranjan <Ashish ranjan@kmg-237>
2017-04-15 11:55:18 +0530

    Immutable class

---
+++ src/com/immutable/MyImmutable.java
@@ -0,0 +1,107 @@
+package com.immutable;
+import java.sql.DriverManager;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Observable;
+import java.util.Observer;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.Vector;
+import java.util.concurrent.atomic.AtomicReference;
+
+import com.myInterface.Animal;
+import com.myInterface.Cow;
+import com.myInterface.Tiger;
+import java.lang.instrument.Instrumentation;
+import java.lang.reflect.Array;
+//Observer design pattern : maintain state check on object.
+interface MyInterface{
+    public int sum();
+}
+abstract class MyAbs implements MyInterface{
+    public int sum(){
+        return 0;
+    }
+}
+class Employee implements Comparable{
+    private String name;
+    private int age;
+    static int count =0;
+    public Employee(){//throws Exception{
+//        if(count++==2){
+//            throw new Exception("Object reach max...");
+//        }
+    }
+    public Employee(String str)throws Exception{  
+        //this(); 
+        this.name=str;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    @Override
+    public String toString(){
+        return this.name;
+    }
+    
+    public int compareTo(Object o) {
+        // TODO Auto-generated method stub
+        Employee emp=(Employee)o;
+        return name.compareTo(emp.getName());
+    } 
+}
+ 
+public class MyImmutable  {//extends MyAbs implements MyInterface{ 
+	public static void main(String[] args) { 
+
+	}
+	
+   
+}
+
+final class Student{
+    private final String name; 
+    private final Employee employee;
+    private final List<Employee> lstData;
+    /*private Student(){
+        
+         * Multiple markers at this line
+            - The blank final field lstData may not have been initialized
+            - The blank final field name may not have been initialized
+            - The blank final field employee may not have been initialized
+         
+    }*/
+    public Student(String name, Employee employee){
+        this.name=name;
+        this.employee=employee;
+        this.lstData=new ArrayList<Employee>();
+    }
+    
+    public String getName() {
+        return name;
+    }
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public List<Employee> getLstData() {
+        return Collections.unmodifiableList(lstData);// throw exception if try to modify : java.lang.UnsupportedOperationException
+    }
+}
