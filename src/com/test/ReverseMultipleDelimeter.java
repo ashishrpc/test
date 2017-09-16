c656355e7545d36bfd81323a3cf791a89d0275a8
Ashish ranjan <Ashish ranjan@kmg-237>
2017-09-16 23:12:32 +0530

    tt

--- src/com/test/ReverseMultipleDelimeter.java
+++ src/com/test/ReverseMultipleDelimeter.java
@@ -4,7 +4,22 @@ public class ReverseMultipleDelimeter {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		final String str="My, Name. is Ashish_";
+		String out="";// "yM, emaN. si hsihsA_"
+		String strin="";
+		char chara;
+		for(int i=0;i<str.length();i++){
+			chara=str.charAt(i);
+			if(chara=='.' || chara==',' || chara==' ' || chara=='_' ){
+				strin=strin+chara;
+				out=out+strin;
+				strin="";
+			}else{
+				strin=chara+strin;
+			}
+		}
+		System.out.println(out);		
 	}
 
 }
