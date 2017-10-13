c409ed0a8c6d32eae2b8000d1e1650602d0fe6c0
Ashish ranjan <Ashish ranjan@kmg-237>
2017-10-13 23:28:58 +0530

    checkNumberIsEvenOdd

--- src/com/dataStructure/SwapTwoNumberWithoutAnyTempVar.java
+++ src/com/dataStructure/SwapTwoNumberWithoutAnyTempVar.java
@@ -5,8 +5,12 @@ public class SwapTwoNumberWithoutAnyTempVar {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//SwapTwoNumberWithoutAnyTempVar(4,6);
+		checkNumberIsEvenOdd(14);
 	}
 
+	public static void checkNumberIsEvenOdd(int num){
+		System.out.println("check Odd??"+ ((num & 0x1) == 1));
+	}
 	public static void SwapTwoNumberWithoutAnyTempVar(int a, int b){
 		System.out.println(a+" before "+b);
 		a^=b;
